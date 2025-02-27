9088b5fcfb25c4ad4705442c34da250778db3e55
Marion <wekesamarion100@gmail.com>
2025-02-27 20:30:37 +0300

    done with the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
-import Testimonial from "./components/Testimonial";
-import SpendWiseHero from "./components/SpendWiseHero";
-import SpendWiseNewsletter from "./components/SpendWiseNewsletter";
-import ConnectWithUs from "./components/ConnectWithUs";
-import MapDisplay from "./components/MapDisplay";
 
 function App() {
   return (
@@ -13,13 +8,8 @@ function App() {
         <NavBar />
       </header>
 
-      <main className="container mx-auto p-6">
+      <main>
         <Outlet />
-        <Testimonial />
-        <SpendWiseHero />
-        <SpendWiseNewsletter />
-        <ConnectWithUs />
-        <MapDisplay />
       </main>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import Hero from "../components/Hero";
+import Testimonial from "../components/Testimonial";
+import SpendWiseHero from "../components/SpendWiseHero";
+import SpendWiseNewsletter from "../components/SpendWiseNewsletter";
+import ConnectWithUs from "../components/ConnectWithUs";
+import MapLayout from "../components/MapLayout";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Testimonial />
+      <SpendWiseHero />
+      <SpendWiseNewsletter />
+      <ConnectWithUs />
+      <MapLayout />
     </>
   );
 };
